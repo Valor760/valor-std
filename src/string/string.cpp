afc930ffdf16a17f68dbdb08c2207ab5305ea6ea
valor760 <valorcool@gmail.com>
2023-02-19 18:40:51 +0200

    Added string::replace

--- src/string/string.cpp
+++ src/string/string.cpp
@@ -1,5 +1,33 @@
 #include "string.h"
 
+#include <stdexcept>
+
+
 namespace vlr {
-	
+	string& string::replace(const std::string& what, const std::string& with, const size_t& n) {
+		if(n < 1) {
+			throw std::runtime_error("Number of replacements (n) cannot be less than 1!");
+		}
+
+		size_t count = 0;
+		size_t pos = 0;
+		while(count < n) {
+			pos = this->find(what, pos);
+			if(pos == this->npos) break;
+
+			std::string::replace(pos, what.length(), with);
+			count++;
+		}
+		return *this;
+	}
+
+	string& string::replace(const char& what, const std::string& with, const size_t& n) {
+		return replace(std::string(&what), with, n);
+	}
+	string& string::replace(const std::string& what, const char& with, const size_t& n) {
+		return replace(what, std::string(&with), n);
+	}
+	string& string::replace(const char& what, const char& with, const size_t& n) {
+		return replace(std::string(&what), std::string(&with), n);
+	}
 } // namespace vlr
--- src/string/string.h
+++ src/string/string.h
@@ -6,7 +6,19 @@
 namespace vlr {
 	class string : public std::string {
 		public:
-			
+			// Call base class constructors
+			string() : std::string() {}
+			string(std::string&& other) : std::string(other) {}
+			string(std::string& other) : std::string(other) {}
+			string(const char* s) : std::string(s) {}
+
+			// Replace functions
+			// TODO: make a doc and export to api
+			string& replace(const std::string& what, const std::string& with, const size_t& n = 1);
+			string& replace(const char& what, const std::string& with, const size_t& n = 1);
+			string& replace(const std::string& what, const char& with, const size_t& n = 1);
+			string& replace(const char& what, const char& with, const size_t& n = 1);
+
 		private:
 	};
 } // namespace vlr
