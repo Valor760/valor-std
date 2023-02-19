f478fb053e1e9fc78143626decadcf930e20b8bd
valor760 <valorcool@gmail.com>
2023-02-19 13:12:59 +0200

    Project setup

---
+++ src/string/string.cpp
@@ -0,0 +1,5 @@
+#include "string.h"
+
+namespace vlr {
+	
+} // namespace vlr
---
+++ src/string/string.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+
+namespace vlr {
+	class string : public std::string {
+
+	};
+} // namespace vlr
