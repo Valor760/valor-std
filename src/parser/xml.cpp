5f5eec562d998d42f4319f22fc1d678e91348fb1
stanislavsn1 <stanislavsn1@verifone.com>
2023-03-16 16:38:23 +0200

    Parser

--- src/parser/xml.cpp
+++ src/parser/xml.cpp
@@ -1,6 +1,52 @@
 #include "xml.h"
 
+#include <fstream>
+
 
 namespace vlr {
+
+XMLNode* XMLParser::getRoot() {
+	if(m_RootNode == nullptr) {
+		try {
+			parseXML();
+		} catch(std::exception& e) {
+			return nullptr;
+		}
+	}
+	return m_RootNode.get();
+}
+
+void XMLParser::parseXML() {
+	if(m_FilePath.empty()) {
+		throw std::runtime_error("No filepath provided!");
+	}
+
+	std::fstream file(m_FilePath);
+	if(!file.is_open()) {
+		throw std::runtime_error("Failed to open file -> " + m_FilePath);
+	}
+
+	// Actual parsing
+	std::string line = "";
+	while(std::getline(file, line)) {
+		// It is possible, that xml is not formated, so each node is not on it's line
+		// So, we need to parse each character
+		for(size_t i = 0; i < line.length(); i++) {
+
+			// Parse open tag
+			if(line[i] == '<') {
+
+				// Find closing tag
+				size_t close_tag_pos = line.find_first_of('>', i);
+
+				// Check for xml prolog
+				if(line[i + 1] == '?') {
+					
+				}
+			}
+		}
+	}
+
+}
 
 } // namespace vlr
--- src/parser/xml.h
+++ src/parser/xml.h
@@ -1,11 +1,46 @@
 #pragma once
 
 #include <string>
-#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <memory>
 
 
 namespace vlr {
-	class XMLParser {
-		XMLParser(const std::string& filepath);
-	};
+
+// TODO: Move node class to cpp file as it is not required to be in public API
+class XMLNode {
+	private:
+		XMLNode(const std::string& name) : m_NodeName(name) {}
+
+		void addChild(const XMLNode& child);
+		XMLNode& getChild(const std::string& child_name);
+		void addAttribute(const std::string& attribute, const std::string& value);
+
+
+	private:
+		const std::string m_NodeName;
+		std::string m_Data = "";
+		std::vector<XMLNode> m_Children = {};
+		std::unordered_map<std::string, std::string> m_NodeAttributes = {};
+
+		friend class XMLParser;
+};
+
+class XMLParser {
+	public:
+		XMLParser(const std::string& filepath) : m_FilePath(filepath) {}
+
+		XMLNode* getRoot();
+
+
+	private:
+		void parseXML();
+
+
+	private:
+		const std::string m_FilePath;
+		std::unique_ptr<XMLNode> m_RootNode = nullptr;
+};
+
 } // namespace vlr
